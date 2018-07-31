02d028245fee5ab66c00c7cba1dfa67f378c8a4c
VitinNote <VitinNote@179.190.77.171>
2018-07-30 21:27:32 -0300

    #1 Estrutura de teste para o git

---
+++ Linguini/src/main/Linguini.java
@@ -0,0 +1,15 @@
+package main;
+
+import view.Hello;
+
+/**
+ *
+ * @author VitinNote
+ */
+public class Linguini {
+
+    public static void main(String[] args) {
+        Hello tela = new Hello();
+        tela.setVisible(true);
+    }
+}
