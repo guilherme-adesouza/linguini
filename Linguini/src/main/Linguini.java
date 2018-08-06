2c1912124ef4973a65134b4787104816c40fa04b
VitinNote <VitinNote@179.190.77.171>
2018-08-06 01:53:23 -0300

    #2 View da tela principal criada

--- Linguini/src/main/Linguini.java
+++ Linguini/src/main/Linguini.java
@@ -1,6 +1,7 @@
 package main;
 
-import view.Hello;
+import view.Home;
+import view.jfLogin;
 
 /**
  *
@@ -9,7 +10,24 @@ import view.Hello;
 public class Linguini {
 
     public static void main(String[] args) {
-        Hello tela = new Hello();
-        tela.setVisible(true);
+         try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(jfLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(jfLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(jfLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(jfLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        
+        new jfLogin().setVisible(true);
     }
 }
