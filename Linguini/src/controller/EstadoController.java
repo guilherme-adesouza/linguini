a0d67def07937955489e47b790d2bc5ec44ed5e1
Vitor <laranjinhacrazy@hotmail.com>
2018-08-16 01:36:37 -0300

    #20 Crud e controller de estados criados

---
+++ Linguini/src/controller/EstadoController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import dao.EstadoDAO;
+import dao.MensagemRetorno;
+import model.Cidade;
+
+/**
+ *
+ * @author VitinNote
+ */
+
+
+public class EstadoController {
+    
+private EstadoDAO estadoDao;
+
+
+}
---
+++ Linguini/src/dao/EstadoDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import model.Estado;
+
+/**
+ *
+ * @author VitinNote
+ */
+public class EstadoDAO extends GenericoDAO<Estado>{
+   
+}
