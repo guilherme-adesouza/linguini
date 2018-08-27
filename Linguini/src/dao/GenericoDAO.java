e3d8e2c880827aed4d9a1871f10a3b91d9919a72
guilherme-souza <guilherme-souza@guilhermesouza-Aspire-ES1-572>
2018-08-26 23:47:47 -0300

    Ajuste para update Hibernate

--- Linguini/src/dao/GenericoDAO.java
+++ Linguini/src/dao/GenericoDAO.java
@@ -29,7 +29,7 @@ public class GenericoDAO<Object> {
             sessao = HibernateUtil.getSessionFactory().openSession();
             Transaction t = sessao.beginTransaction();
 
-            sessao.save(o);
+            sessao.saveOrUpdate(o);
             t.commit();
 
             retorno.setSucesso(true);
