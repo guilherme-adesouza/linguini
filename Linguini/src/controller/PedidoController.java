0cb79d1aaa6b08effd3ecd193ed6a241d355a5fa
VitinGroot <laranjinhacrazy@hotmail.com>
2018-10-01 21:01:58 -0300

    #36 Continuando

--- Linguini/src/controller/PedidoController.java
+++ Linguini/src/controller/PedidoController.java
@@ -2,8 +2,11 @@ package controller;
 
 import dao.MensagemRetorno;
 import dao.PedidoDAO;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import model.Pedido;
 
 /**
@@ -35,7 +38,84 @@ public class PedidoController implements Controller<Pedido>{
 
     @Override
     public void popularTabela(JTable tabela, String criterio, String ordenacao) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+// dados da tabela
+        Object[][] dadosTabela = null;
+
+        // cabecalho da tabela
+        String[] cabecalho = this.getCabecalho();
+
+        // cria matriz de acordo com nº de registros da tabela
+        try {
+            MensagemRetorno ms = this.pedidoDAO.consultarAtivosComCriterio(this.tabela, this.getCamposPesquisaveis(), criterio, ordenacao);
+            if (ms.isSucesso()) {
+                dadosTabela = new Object[ms.getLista().size()][cabecalho.length];
+            }
+            dadosTabela = new Object[ms.getLista().size()][4];
+
+        } catch (Exception e) {
+            System.out.println("Erro ao consultar Fornecedor: " + e);
+        }
+
+        int lin = 0;
+
+        // efetua consulta na tabela
+        try {
+            MensagemRetorno ms = this.pedidoDAO.consultarAtivosComCriterio(this.tabela, this.getCamposPesquisaveis(), criterio, ordenacao);
+
+            for (Object o : ms.getLista()) {
+                Pedido p = (Pedido) o;
+                dadosTabela[lin][0] = p.getId();
+                dadosTabela[lin][1] = p.getValor();
+                lin++;
+            }
+
+            // caso a coluna precise exibir uma imagem
+//                if (resultadoQ.getBoolean("Situacao")) {
+//                    dadosTabela[lin][2] = new ImageIcon(getClass().getClassLoader().getResource("Interface/imagens/status_ativo.png"));
+//                } else {
+//                    dadosTabela[lin][2] = new ImageIcon(getClass().getClassLoader().getResource("Interface/imagens/status_inativo.png"));
+//                }
+        } catch (Exception e) {
+            System.out.println("problemas para popular tabela Fornecedor...");
+            System.out.println(e);
+        }
+
+        // configuracoes adicionais no componente tabela
+        tabela.setModel(new DefaultTableModel(dadosTabela, cabecalho) {
+            @Override
+            // quando retorno for FALSE, a tabela nao é editavel
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+
+            // alteracao no metodo que determina a coluna em que o objeto ImageIcon devera aparecer
+            @Override
+            public Class
+                    getColumnClass(int column) {
+
+                if (column == 2) {
+                    //return ImageIcon.class;
+                }
+                return Object.class;
+            }
+        });
+
+        // permite seleção de apenas uma linha da tabela
+        tabela.setSelectionMode(0);
+
+        // redimensiona as colunas de uma tabela
+        TableColumn column = null;
+        for (int i = 0; i < tabela.getColumnCount(); i++) {
+            column = tabela.getColumnModel().getColumn(i);
+            switch (i) {
+                case 0:
+                    column.setPreferredWidth(17);
+                    break;
+                case 1:
+                    column.setPreferredWidth(140);
+                    break;
+            }
+        }
     }
 
     @Override
@@ -50,7 +130,18 @@ public class PedidoController implements Controller<Pedido>{
 
     @Override
     public List<CampoOrdenavel> getOrdenacao() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List campos = new ArrayList();
+        campos.add(new CampoOrdenavel("ID", "id"));
+        campos.add(new CampoOrdenavel("Valor Total", "valor"));
+        return campos;}
+
+    private String[] getCabecalho() {
+        String[] cabecalho = {"Código", "Valor Total"};
+        return cabecalho;
+    }
+    private String[] getCamposPesquisaveis() {
+        String[] campos = {"CAST(id AS text)", "id"};
+        return campos;
     }
     
 }
