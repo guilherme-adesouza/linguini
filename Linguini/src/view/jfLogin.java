4738093d86a6cb15783a1b44ada9231850f6c19e
guilherme-souza <guilherme-souza@guilhermesouza-Aspire-ES1-572>
2018-12-08 16:14:56 -0200

    Ajuste XML tela de login
    - Tela de login não abria para edição no modo design (Swing)

--- Linguini/src/view/jfLogin.java
+++ Linguini/src/view/jfLogin.java
@@ -10,8 +10,6 @@ import dao.MensagemRetorno;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.AbstractAction;
 import javax.swing.InputMap;
 import javax.swing.JComponent;
